1deae2891d26d964c98b33cbb4be5b6c9012405d
Christian Bumann <bch@bbtsoftware.ch>
2018-12-03 15:18:32 +0100

    Implement creating a new Jira issue

---
+++ src/Cake.Jira/Dtos/CreateIssueSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cake.Jira.Dtos
+{
+    public class CreateIssueSettings : JiraSettings
+    {
+        public string Project { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Reporter { get; set; }
+        public string Environment { get; set; }
+        public string Assignee { get; set; }
+        public int Priority { get; set; }
+        public int Type { get; set; }
+        public List<string> Labels { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
